e2d37b820773abb77a478a2b452313b04fc8760d
0tjLustig <55846769+0tjLustig@users.noreply.github.com>
2020-12-07 21:11:51 -0600

    Update Lunch44.js
    Fixed Issue main splash page not rendering after Login

--- Lunch44.js
+++ Lunch44.js
@@ -4,17 +4,20 @@ var app = express();
 
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-  host: "45.55.136.114:3306",
-  user: "JakeJosh_F2020",
-  password: "pizza1234",
-  database: "JakeJosh_F2020"
+const connection = mysql.createConnection({
+    host: "45.55.136.114",
+    user: "JakeJosh_F2020",
+    password: "pizza1234",
+    database: "JakeJosh_F2020"
 });
 
-//con.connect(function(err) {
-  //if (err) throw err;
-  //console.log("Connected!");
-//});
+connection.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('Connected to MySQL DBMS...!');
+});
+
 
 
 app.set( 'view engine', 'pug');
@@ -42,11 +45,38 @@ app.post('/login', function(req, res){
 app.use( bodyParser.urlencoded({ extended:true }));
 
 app.post('/myPost', function(req, res){
-    res.render('afterPage');
-    console.log('Got Body:', req.body);
-    let name = req.body.Name;
-    
+    connection.query("select * from LunchGroup where GroupID = '1'", function(err2, res2){
+        if(err2) throw err2;
+        console.log("we ok");
+        let data = {};
+        for(let i in res2){
+                //some code that takes the request from login
+                //and displays results i guess
+            data = {
+                id : res2[i].GroupID,
+                date : res2[i].GroupDate,
+                leaderName : res2[i].GroupLeader_UName,
+                    //leaderDept : res2[i].User_Department,
+                leaderEmail : res2[i].GroupLeader_EMail,
+                gr1N : res2[i].GroupMember1_UName,
+                gr1E : res2[i].GroupMember1_EMail,
+                gr2N : res2[i].GroupMember2_UName,
+                gr2E : res2[i].GroupMember2_EMail,
+                gr3N : res2[i].GroupMember3_UName,
+                gr3E : res2[i].GroupMember3_EMail
+            }
+        }
+        if(err2) throw err2;
+        console.log(data);
+        res.render('afterPage', data);
+
+    })
+
 });
+app.get('/myPost', function(req, res){
+    res.render('afterPage')
+});
+
 app.post('/settings', function(req,res){
     res.render('settingsPage')
 });
